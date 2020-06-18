68c70827b1c5587ca0bf16c7569e5a76093ab801
Janderson Pizzially Vilas Boas <jandersonpizziallyvilasboas@gmail.com>
2020-06-18 19:54:08 -0300

    chore: Estilizando o Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,49 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/25486388?s=460&u=b9bffd30c4bdf0adcfc73a8893ca204b81a77350&v=4"
+            alt="jandersonp"
+          />
+          <div>
+            <strong>jandersonp/gobarber-api</strong>
+            <p>API REST da aplicação GoBarber</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/25486388?s=460&u=b9bffd30c4bdf0adcfc73a8893ca204b81a77350&v=4"
+            alt="jandersonp"
+          />
+          <div>
+            <strong>jandersonp/gobarber-api</strong>
+            <p>API REST da aplicação GoBarber</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
